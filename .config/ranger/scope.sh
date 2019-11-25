4cb04314a8a428bc763cbacf0a48f8d3520ad40a
hydrocodone <44106031+diracs-delta@users.noreply.github.com>
2019-11-25 15:08:11 -0600

    use GNU source-highlight for text preview

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -34,9 +34,6 @@ FILE_EXTENSION="${FILE_PATH##*.}"
 FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
 
 # Settings
-HIGHLIGHT_SIZE_MAX=262143  # 256KiB
-HIGHLIGHT_TABWIDTH=8
-HIGHLIGHT_STYLE='pablo'
 PYGMENTIZE_STYLE='autumn'
 
 
@@ -130,21 +127,8 @@ handle_mime() {
     case "${mimetype}" in
         # Text
         text/* | */xml)
-            # Syntax highlight
-            if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
-                exit 2
-            fi
-            if [[ "$( tput colors )" -ge 256 ]]; then
-                local pygmentize_format='terminal256'
-                local highlight_format='xterm256'
-            else
-                local pygmentize_format='terminal'
-                local highlight_format='ansi'
-            fi
-            highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
-                --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
-            # pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}" -- "${FILE_PATH}" && exit 5
-            exit 2;;
+            source-highlight-esc.sh "${FILE_PATH}" && exit 5
+	    exit 2;;
 
         # Image
         image/*)
